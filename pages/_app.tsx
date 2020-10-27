89409852ed4b904863c06a717fc3a3b6d31480cc
Max Isom <hi@maxisom.me>
2020-10-27 19:59:40 -0400

    Bump dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import {AppProps} from 'next/app';
 import Head from 'next/head';
 import {config} from '@fortawesome/fontawesome-svg-core';
