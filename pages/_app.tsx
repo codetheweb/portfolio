205f55961dbf1dbb46aad1dcbe11202b1a6cd857
Max Isom <hi@maxisom.me>
2021-10-14 13:21:49 -0400

    Add alias for Plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ const MyApp = ({Component, pageProps}: AppProps) => {
 			<Head>
 				<title>Max Isom</title>
 				<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"/>
-				<script async defer data-domain="maxisom.me" src="https://plause.maxisom.me/js/plausible.js"/>
+				<script async defer data-domain="maxisom.me" src="https://plause.maxisom.me/js/plause.js"/>
 
 				<link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png"/>
 				<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png"/>
