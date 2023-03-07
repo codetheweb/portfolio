6e9afda055a883d2655a929d85c09af46f6e4d2e
Max Isom <hi@maxisom.me>
2023-03-06 23:31:13 -0800

    Custom MDX header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './styles/header.module.scss';
 
-export default function Header({size, children, topMargin = false}: {size: 'h1' | 'h2' | 'h3'; children: React.ReactNode; topMargin?: boolean}) {
+export default function Header({size, children, topMargin = false, id}: {size: 'h1' | 'h2' | 'h3'; children: React.ReactNode; topMargin?: boolean; id?: string}) {
 	const H = size;
 
 	return (
-		<H className={`${styles.header} ${topMargin ? styles.topMargin : ''}`}>{children}</H>
+		<H className={`${styles.header} ${topMargin ? styles.topMargin : ''}`} id={id}>{children}</H>
 	);
 }
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,3 +1,26 @@
+import React from 'react';
 import type {MDXComponents} from 'mdx/types';
+import Header from './components/header';
+import TextLink from './components/text-link';
 
-export const useMDXComponents = (components: MDXComponents): MDXComponents => components;
+const getHeaderId = (children: React.ReactNode) => children?.toString().toLocaleLowerCase().replaceAll(' ', '-');
+
+const WrappedHeader = (props: any) => {
+	const id = getHeaderId(props.children);
+
+	return (
+		<Header size={props.size as 'h1'} id={id}>
+			<a href={`#${id ?? ''}`}>
+				{props.children}
+			</a>
+		</Header>
+	);
+};
+
+export const useMDXComponents = (components: MDXComponents): MDXComponents => ({
+	...components,
+	a: (props: any) => <TextLink {...props}/>,
+	h1: props => <WrappedHeader size="h1">{props.children}</WrappedHeader>,
+	h2: props => <WrappedHeader size="h2">{props.children}</WrappedHeader>,
+	h3: props => <WrappedHeader size="h3">{props.children}</WrappedHeader>,
+});
