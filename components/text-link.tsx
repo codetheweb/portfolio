e0300c5894c5002d0ffaabc56461178d9b903a78
Max Isom <hi@maxisom.me>
2023-03-07 21:32:24 -0800

    Fix footnotes

--- components/text-link.tsx
+++ components/text-link.tsx
@@ -5,27 +5,28 @@ import styles from './styles/text-link.module.scss';
 
 type Props = {
 	className?: string;
+	id?: string;
 	href?: string;
 	children: React.ReactNode;
 	onClick?: DOMAttributes<HTMLAnchorElement>['onClick'];
 };
 
-export default function TextLink({href, onClick, children, className}: Props) {
+export default function TextLink({href, onClick, children, className, id}: Props) {
 	if (onClick) {
 		return (
-			<a className={classNames(className, styles.link)} href={href} onClick={onClick}>{children}</a>
+			<a id={id} className={classNames(className, styles.link)} href={href} onClick={onClick}>{children}</a>
 		);
 	}
 
-	if (href?.startsWith('/')) {
+	if (href?.startsWith('/') || href?.startsWith('#')) {
 		return (
-			<Link href={href} className={classNames(className, styles.link)}>
+			<Link href={href} className={classNames(className, styles.link)} id={id}>
 				{children}
 			</Link>
 		);
 	}
 
 	return (
-		<a href={href} target="_blank" rel="noreferrer" className={classNames(className, styles.link)}>{children}</a>
+		<a href={href} id={id} target="_blank" rel="noreferrer" className={classNames(className, styles.link)}>{children}</a>
 	);
 }
