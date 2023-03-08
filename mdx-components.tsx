ca7f59f9b5e7ca3d5f844d7f74631629e8b36a9e
Max Isom <hi@maxisom.me>
2023-03-07 22:14:24 -0800

    Small edits

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type {MDXComponents} from 'mdx/types';
-import Header from './components/header';
+import classNames from 'classnames';
 import TextLink from './components/text-link';
 
 const getHeaderId = (children: React.ReactNode) => children?.toString().toLocaleLowerCase().replaceAll(' ', '-');
@@ -8,18 +8,22 @@ const getHeaderId = (children: React.ReactNode) => children?.toString().toLocale
 const WrappedHeader = (props: any) => {
 	const id = getHeaderId(props.children);
 
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+	const H = props.size;
+
 	return (
-		<Header size={props.size as 'h1'} id={id}>
+		<H id={id}>
 			<a href={`#${id ?? ''}`}>
 				{props.children}
 			</a>
-		</Header>
+		</H>
 	);
 };
 
 export const useMDXComponents = (components: MDXComponents): MDXComponents => ({
 	...components,
 	a: (props: any) => <TextLink {...props}/>,
+	pre: props => <pre {...props} className={classNames(props.className, 'disable-default-code-styles')}/>,
 	h1: props => <WrappedHeader size="h1">{props.children}</WrappedHeader>,
 	h2: props => <WrappedHeader size="h2">{props.children}</WrappedHeader>,
 	h3: props => <WrappedHeader size="h3">{props.children}</WrappedHeader>,
