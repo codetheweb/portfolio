f51d9869d80bea0f7b1c1ea6d85ca4d8a577a2b0
Max Isom <hi@maxisom.me>
2025-05-26 11:31:20 -0700

    ✨ bounce

--- components/hovering-home-button.tsx
+++ components/hovering-home-button.tsx
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import Link from 'next/link';
 import {Home} from 'react-feather';
 import styles from './styles/hovering-home-button.module.scss';
 
-export const HoveringHomeButton = () => (
-	<Link href="/" className={styles.homeButtonContainer} aria-label="go home">
-		<span className={styles.homeButtonIcon}>
-			<Home/>
-		</span>
-	</Link>
-);
+export const HoveringHomeButton = () => {
+	const pageBottomRef = useRef<HTMLDivElement>(null);
+	const [bounce, setBounce] = useState(false);
+	const [hasBouncedAtBottom, setHasBouncedAtBottom] = useState(false);
+
+	useEffect(() => {
+		if (!bounce) {
+			return;
+		}
+
+		const timeout = setTimeout(() => {
+			setBounce(false);
+		}, 1000);
+		return () => {
+			clearTimeout(timeout);
+		};
+	}, [bounce]);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			if (pageBottomRef.current && !hasBouncedAtBottom) {
+				const rect = pageBottomRef.current.getBoundingClientRect();
+				if (rect.top <= window.innerHeight + 800 && rect.bottom >= 0) {
+					setBounce(true);
+					setHasBouncedAtBottom(true);
+				}
+			}
+		};
+
+		window.addEventListener('scroll', handleScroll);
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, [hasBouncedAtBottom]);
+
+	return (
+		<>
+			<div ref={pageBottomRef}/>
+			<Link href="/" className={`${styles.homeButtonContainer} ${bounce ? styles.bounce : ''}`} aria-label="go home">
+				<span className={styles.homeButtonIcon}>
+					<Home/>
+				</span>
+			</Link>
+		</>
+	);
+};
